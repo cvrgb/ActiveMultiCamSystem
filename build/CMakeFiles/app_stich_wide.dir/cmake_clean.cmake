f9ab249e25894ce60e3bd88bcdf371f0789523ec
cvrgb <cvrgb@vivaldi.net>
2022-02-09 09:36:19 +0800

    Initial commit

---
+++ build/CMakeFiles/app_stich_wide.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "../bin/app_stich_wide"
+  "../bin/app_stich_wide.pdb"
+  "CMakeFiles/app_stich_wide.dir/examples/app_stich_wide.cpp.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/Edge.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/FloatImage.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/GLine2D.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/GLineSegment2D.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/Gaussian.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/GrayModel.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/Homography33.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/MathUtil.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/Quad.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/Segment.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/TagDetection.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/TagDetector.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/TagFamily.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/UnionFindSimple.cc.o"
+  "CMakeFiles/app_stich_wide.dir/modules/apriltags/src/gridDetector.cpp.o"
+  "CMakeFiles/app_stich_wide.dir/modules/calibrateCamera/src/Zhang.cpp.o"
+  "CMakeFiles/app_stich_wide.dir/modules/calibrateCamera/src/Zhang_DLT.cpp.o"
+  "CMakeFiles/app_stich_wide.dir/modules/calibrateCamera/src/Zhang_nonlinear.cpp.o"
+  "CMakeFiles/app_stich_wide.dir/modules/common/src/common.cpp.o"
+  "CMakeFiles/app_stich_wide.dir/modules/handeye/src/handeye.cpp.o"
+  "CMakeFiles/app_stich_wide.dir/modules/handeye/src/handeye_sfm.cpp.o"
+  "CMakeFiles/app_stich_wide.dir/modules/kinematics/src/kinematicsX1X2.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app_stich_wide.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/calib_aprilgrid.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "../bin/calib_aprilgrid"
+  "../bin/calib_aprilgrid.pdb"
+  "CMakeFiles/calib_aprilgrid.dir/examples/calib_aprilgrid.cpp.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/Edge.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/FloatImage.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/GLine2D.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/GLineSegment2D.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/Gaussian.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/GrayModel.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/Homography33.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/MathUtil.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/Quad.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/Segment.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/TagDetection.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/TagDetector.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/TagFamily.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/UnionFindSimple.cc.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/apriltags/src/gridDetector.cpp.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/calibrateCamera/src/Zhang.cpp.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/calibrateCamera/src/Zhang_DLT.cpp.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/calibrateCamera/src/Zhang_nonlinear.cpp.o"
+  "CMakeFiles/calib_aprilgrid.dir/modules/common/src/common.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calib_aprilgrid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
